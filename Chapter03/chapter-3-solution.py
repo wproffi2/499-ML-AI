9848717962c692434c32b0f1f4b9d7a9d996e2f4
warrie <warrie.proffitt@basecampcs.com>
2020-02-15 12:44:29 -0500

    updated chapter 3 solution

--- Chapter03/chapter-3-solution.py
+++ Chapter03/chapter-3-solution.py
@@ -1,9 +1,24 @@
 import pandas
+import sklearn
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, AdaBoostClassifier, DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import cross_validation
+import time
 
-malDataSet = pandas.read_csv('Android_Feats.csv',low_memory=False)
+
+malDataSet = pandas.read_csv('dataset.csv',low_memory=False)
 
 origin_headers = list(malDataSet.columns.values)
 
 total_data = malDataSet[origin_headers[:-1]]
 total_data = total_data.as_matrix()
 target_strings = malDataSet[origin_headers[-1]]
+
+train, test, target_train, target_test = train_test_split(total_data, target_strings, test_size=0.33, random_state=int(time.time()))
+
+classifiers = [
+    RandomForestClassifier(n_estimators=100),
+    DecisionTreeClassifier(),
+    AdaBoostClassifier()
+]
